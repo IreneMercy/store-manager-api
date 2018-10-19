e0c7c5d49e94760f2ebcbb105a25c202812e4c28
IreneMercy <irenemercy700@gmail.com>
2018-10-19 15:28:47 +0300

    [#161248595]Create admin add product endpoint

---
+++ app/api/v1/views.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify,request, make_response
+from flask_restful import Resource, Api
+
+app = Flask (__name__)
+api = Api(app)
+
+product_details = []
+class Products(Resource):
+
+    def post(self):
+        data = request.get_json()
+        id=len(product_details)+1
+        product_category = data['Category']
+        product_name = data['Name']
+        product_description = data['Description']
+        product_quantity = data['Quantity']
+        price = data['Price']
+        expected_returns = int(price) * int(product_quantity)
+
+        new_product = {
+                    "id":id,
+                    "Category":product_category,
+                    "Name":product_name,
+                    "Description":product_description,
+                    "Quantity":product_quantity,
+                    "Price":price,
+                    "Expected Returns":expected_returns
+        }
+
+        product_details.append(new_product)
+        return make_response( jsonify({"product details":product_details}), 201)
