45f77f6d6f152aebf337469de69f06905e52d7cb
IreneMercy <irenemercy700@gmail.com>
2018-10-19 17:25:05 +0300

    [#161328494]Create test for get specific-sale-order

--- app/tests/test_run.py
+++ app/tests/test_run.py
@@ -38,3 +38,7 @@ class AddProduct(unittest.TestCase):
     def test_get_SaleRecords(self):
         result=self.app.get('/salerecords', content_type="application/json")
         self.assertEqual(result.status_code, 200)
+
+    def test_get_SpecificSale(self):
+        result=self.app.get('/salerecords/2', content_type="application/json")
+        self.assertEqual(result.status_code, 200)
