dd89c36e0a270814bac1d15fed375841ba7b0057
IreneMercy <irenemercy700@gmail.com>
2018-10-19 17:11:27 +0300

    [#161248970]Create test for get specific product

--- app/tests/test_run.py
+++ app/tests/test_run.py
@@ -13,10 +13,28 @@ class AddProduct(unittest.TestCase):
                             "Quantity" : 782,
                             "Price" : 1200
                             }
+        self.new_sale_order={
+                            "Category":"Max",
+                            "Name":"Fox",
+                            "Quantity":6,
+                            "Price":1000
+                        }
     def test_post_Products(self):
         result=self.app.post('/products', data=json.dumps(self.new_product), content_type="application/json")
         self.assertEqual(result.status_code, 201)
 
     def test_get_products(self):
         result=self.app.get('/products', content_type="application/json")
+        self.assertEqual(result.status_code, 200)
+
+    def test_get_specificproduct(self):
+        result=self.app.get('/products/1', content_type="application/json")
+        self.assertEqual(result.status_code, 200)
+
+    def test_post_SaleRecords(self):
+        result=self.app.post('/salerecords', data=json.dumps(self.new_sale_order), content_type="application/json")
+        self.assertEqual(result.status_code, 201)
+
+    def test_get_SaleRecords(self):
+        result=self.app.get('/salerecords', content_type="application/json")
         self.assertEqual(result.status_code, 200)
