87de7277e2b77d77aac40243903bc4999998218f
IreneMercy <irenemercy700@gmail.com>
2018-10-19 17:00:28 +0300

    [#161248646]modify get all products

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -6,9 +6,9 @@ api = Api(app)
 
 product_details = []
 class Products(Resource):
- def test_get_products(self):
     def get(self):
         return make_response(jsonify({"product_details":product_details}), 200)
+
     def post(self):
         data = request.get_json()
         id=len(product_details)+1
@@ -31,3 +31,28 @@ class Products(Resource):
 
         product_details.append(new_product)
         return make_response( jsonify({"product details":product_details}), 201)
+
+sale_records=[]
+class SaleRecord(Resource):
+    def get(self):
+        return make_response(jsonify({"sale_records":sale_records}), 200)
+    def post(self):
+        data = request.get_json()
+        id=len(sale_records)+1
+        product_category = data['Category']
+        product_name = data['Name']
+        product_quantity = data['Quantity']
+        price = data['Price'] 
+        expected_returns = int(price) * int(product_quantity)
+
+        new__sale_order = {
+                    "id":id,
+                    "Category":product_category,
+                    "Name":product_name,
+                    "Quantity":product_quantity,
+                    "Price":price,
+                    "Expected Returns":expected_returns
+        }
+
+        sale_records.append(new__sale_order)
+        return make_response( jsonify({"sale records":sale_records}), 201)
